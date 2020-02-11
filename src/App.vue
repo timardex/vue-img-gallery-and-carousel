317d43fb54ece787da5700c23004b5abe8543d33
timardex <timardex@gmail.com>
2020-02-11 12:17:15 +0100

    setup app.vue file

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,42 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <Photography :imageList="imageList" />
+  </div>
 </template>
 
 <script>
-export default {};
+import Photography from "@/components/Photography";
+export default {
+  components: {
+    Photography
+  },
+  data: () => ({
+    imageList: []
+  }),
+  mounted() {
+    this.importAll(require.context("./assets/img/", true, /\.jpg$/));
+    console.log(this.imageList)
+  },
+  methods: {
+    importAll(r) {
+      r.keys().forEach((key, id) => {
+        this.imageList.push({
+          path: r(key),
+          name: key
+            .replace("./", "")
+            .replace(".jpg", "")
+            .replace(/-/g, " ")
+            .substr(key.indexOf("-") - 1), // remove './', '.jpg', '-' and first word
+          category: key
+            .replace("./", "")
+            .replace(".jpg", "")
+            .replace(/-/g, " ")
+            .replace(/ .*/, ""), // remove './', '.jpg', '-' and get first word as category
+          id: id,
+          active: false
+        });
+      });
+    }
+  }
+};
 </script>
